db5361108d2d28064ac207762acfbe4536a8c6d3
Diana Ma <diana@rarestype.com>
2026-02-23 00:05:13 +0000

    vite path

--- TypeScript/vite.config.js
+++ TypeScript/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
-    base: '/majesty/',
+    base: '/benchmark/',
     publicDir: '../Public',
     resolve: {
         alias: {
